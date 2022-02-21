716469c9ec63ceaa3e97a35c60391f8a8d1087ab
Antoine Avenia <antoine_avenia@hotmail.fr>
2022-02-21 15:45:47 +0100

    add project entity

---
+++ src/Entity/Project.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProjectRepository::class)]
+class Project
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 45)]
+    private $name;
+
+    #[ORM\Column(type: 'boolean')]
+    private $block_sources;
+
+    #[ORM\Column(type: 'boolean')]
+    private $is_deleted;
+
+    #[ORM\Column(type: 'datetime')]
+    private $date_add;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getBlockSources(): ?bool
+    {
+        return $this->block_sources;
+    }
+
+    public function setBlockSources(bool $block_sources): self
+    {
+        $this->block_sources = $block_sources;
+
+        return $this;
+    }
+
+    public function getIsDeleted(): ?bool
+    {
+        return $this->is_deleted;
+    }
+
+    public function setIsDeleted(bool $is_deleted): self
+    {
+        $this->is_deleted = $is_deleted;
+
+        return $this;
+    }
+
+    public function getDateAdd(): ?\DateTimeInterface
+    {
+        return $this->date_add;
+    }
+
+    public function setDateAdd(\DateTimeInterface $date_add): self
+    {
+        $this->date_add = $date_add;
+
+        return $this;
+    }
+}
