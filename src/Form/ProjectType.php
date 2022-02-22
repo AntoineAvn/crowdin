37b2c98fc45b12c190214cdb3e590dd7b56f4e6b
Antoine Avenia <antoine_avenia@hotmail.fr>
2022-02-22 16:29:34 +0100

    modif fields

--- src/Form/ProjectType.php
+++ src/Form/ProjectType.php
@@ -2,9 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Lang;
 use App\Entity\Project;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProjectType extends AbstractType
@@ -16,9 +18,15 @@ class ProjectType extends AbstractType
             ->add('block_sources')
             ->add('is_deleted')
             ->add('date_add')
-            ->add('user')
+            // ->add('user')
             ->add('lang')
-            ->add('langtotranslate')
+            ->add('langtotranslate', EntityType::class, [
+                'class' => Lang::class, //use to get class name
+                'choice_label' => 'name', //use to get name of lang
+                'expanded' => true,
+                'multiple' => true,
+                'label' => 'Need to be translate in:'
+            ])
         ;
     }
 
