1b6751b5b942c5865b80e974e605d0ae3c54f578
Antoine Avenia <antoine_avenia@hotmail.fr>
2022-02-22 15:21:30 +0100

    create project CRUD

---
+++ src/Form/ProjectType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Project;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjectType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('block_sources')
+            ->add('is_deleted')
+            ->add('date_add')
+            ->add('user')
+            ->add('lang')
+            ->add('langtotranslate')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Project::class,
+        ]);
+    }
+}
