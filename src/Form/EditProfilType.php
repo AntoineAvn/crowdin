11bee84ca34e0d563f9341599a3204afab855e8a
Antoine Avenia <antoine_avenia@hotmail.fr>
2022-02-24 16:01:15 +0100

    create form editProfilType for edit profil user

---
+++ src/Form/EditProfilType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lang;
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EditProfilType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('username')
+            ->add('description')
+            ->add('langhasuser', EntityType::class, [
+                'class' => Lang::class, //use to get class name
+                'choice_label' => 'name', //use to get name of lang
+                'expanded' => true,
+                'multiple' => true
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
