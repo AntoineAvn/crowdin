dffdde4bbbebf3beac1ec8de701a643b668ed4e1
Antoine Avenia <antoine_avenia@hotmail.fr>
2022-03-18 15:43:54 +0100

    add route to show sources by project id

--- src/Controller/TraductionSourceController.php
+++ src/Controller/TraductionSourceController.php
@@ -4,21 +4,29 @@ namespace App\Controller;
 
 use App\Entity\TraductionSource;
 use App\Form\TraductionSourceType;
-use App\Repository\TraductionSourceRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use App\Repository\TraductionSourceRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/traduction/source')]
 class TraductionSourceController extends AbstractController
 {
-    #[Route('/', name: 'traduction_source_index', methods: ['GET'])]
-    public function index(TraductionSourceRepository $traductionSourceRepository): Response
+    // #[Route('/', name: 'traduction_source_index', methods: ['GET'])]
+    // public function index(TraductionSourceRepository $traductionSourceRepository): Response
+    // {
+    //     return $this->render('traduction_source/index.html.twig', [
+    //         'traduction_sources' => $traductionSourceRepository->findAll(),
+    //     ]);
+    // }
+
+    #[Route('/project/{id}', name: 'traduction_source_byproject', methods: ['GET'])]
+    public function showListByProject(TraductionSourceRepository $traductionSourceRepository, int $id): Response
     {
         return $this->render('traduction_source/index.html.twig', [
-            'traduction_sources' => $traductionSourceRepository->findAll(),
+            'traduction_sources' => $traductionSourceRepository->findBy(array('project' => $id)),
         ]);
     }
 
@@ -33,7 +41,7 @@ class TraductionSourceController extends AbstractController
             $entityManager->persist($traductionSource);
             $entityManager->flush();
 
-            return $this->redirectToRoute('traduction_source_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('project_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('traduction_source/new.html.twig', [
